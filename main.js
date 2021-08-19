75e207ef09a98bab262140701439bf657fc58ea4
woohaen88 <woohaen88@gmail.com>
2021-08-19 20:31:22 +0900

    07-variable-scope

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-// 반복문 (for statement)
-// for (시작조건; 종료조건; 변화조건) {}
+// 변수 유효범위(Variable Scope)
+// var, let, const
 
-for (let i = 0; i < 3; i += 1){
-  console.log(i)
+
+// Error
+// function scope() {
+//   if (true){
+//     const a = 123    
+//   }
+//   console.log(a)
+// }
+
+function scope() {
+  if (true){
+    const a = 123    
+    console.log(a)
+  }  
 }
-console.log(ulEl)
+scope()
 
-const ulEl = document.querySelector('ul')
 
-for (let i = 0; i < 10; i += 1){
-  const li = document.createElement('li')
-  li.textContent = `list-${i+1}`
-  if ((i+1) % 2 === 0) {
-    li.addEventListener('click', function () {
-      console.log(li.textContent)
-    })
-  }
-  ulEl.appendChild(li)
+function scope() {
+  if (true){
+    var a = 123  // 함수레벨      
+  } 
+  console.log(a) 
 }
+scope()
