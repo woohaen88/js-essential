838e825a9a65036669fab2b20abb6ba262069c3d
woohaen88 <woohaen88@gmail.com>
2021-08-19 18:00:01 +0900

    ternary-operator

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-// 비교 연산자(arithemetic operator)
+// ternary operator
 
-const a = 1
-const b = 1
+const a = 1 < 2 //true
 
-console.log(a === b) // 일치 
-
-const c = 1
-const d = 2
-
-console.log(c === d) // 불일치 
-
-function isEqual(x ,y) {
-  return x === y
+if (a) {
+  console.log('true')
+}else {
+  console.log('false')
 }
 
-console.log(isEqual(1, 1))
-console.log(isEqual(2, '1'))
-
-const tmp1 = 1
-const tmp2 = 3
-const tmp3 = 1
-
-console.log(tmp1 !== tmp2)
-console.log(tmp1 !== tmp3)
-
-console.log(tmp1 < tmp3)
-console.log(tmp1 < tmp2)
-console.log(tmp1 <= tmp2)
-
-// 논리 연산자(logical operator)
-const aa = 1 === 1 // true
-const bb = 'AB' === 'AB' //true
-const cc = false
-
-console.log(aa)
-console.log(bb)
-console.log(cc)
-
-console.log('&&: ', aa && bb && cc)
-console.log('|| ', aa || bb || cc)
-console.log('!aa: ', !aa)
+console.log(a ? 'true' : 'false')
