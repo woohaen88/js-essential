2a9c442a9d74a12387c5201d1730de7afd58a338
woohaen88 <woohaen88@gmail.com>
2021-08-19 17:42:13 +0900

    operation

---
+++ main.js
@@ -0,0 +1,18 @@
+// 산술 연산자(arithemetic operator)
+
+console.log(1 + 2)
+console.log(5 - 7)
+console.log(3 * 4)
+console.log(10 / 2)
+
+//나머지
+console.log(7 % 5)
+
+// 할당
+let a = 2 
+a = a + 1
+
+console.log(a)
+
+a += 2 // a = a + 2 //  * + / - 적용가능
+console.log(a)
