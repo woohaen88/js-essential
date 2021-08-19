26e39d9936642347fd2d787d430aa7cf99e4dd72
woohaen88 <woohaen88@gmail.com>
2021-08-19 20:12:29 +0900

    05-switch

--- main.js
+++ main.js
@@ -5,13 +5,27 @@ import random from './getRandom'
 const a = random()
 
 
-if (a === 0) {
-  console.log('a is 0')
-} else if (a === 2) {
-  console.log('a is 2')
-} else if (a === 4) {
-  console.log('a is 4')
-} else {
-  console.log('rest....')
+switch (a) {
+  case 0:
+    console.log('a is 0')
+    break // 중요
+  case 2:
+    console.log('a is 2')
+    break
+  case 4:
+    console.log('a is 4')
+    break  
+  default:
+    console.log('rest....')
 }
+
+// if (a === 0) {
+//   console.log('a is 0')
+// } else if (a === 2) {
+//   console.log('a is 2')
+// } else if (a === 4) {
+//   console.log('a is 4')
+// } else {
+//   console.log('rest....')
+// }
 
