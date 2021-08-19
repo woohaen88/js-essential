39e92943d4db271c281d50c1a5f30f8699272015
woohaen88 <woohaen88@gmail.com>
2021-08-19 20:24:38 +0900

    06-forloop

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
-import random from './getRandom'
+// 반복문 (for statement)
+// for (시작조건; 종료조건; 변화조건) {}
 
-// 조건문 (if statement)
-
-const a = random()
-
-
-switch (a) {
-  case 0:
-    console.log('a is 0')
-    break // 중요
-  case 2:
-    console.log('a is 2')
-    break
-  case 4:
-    console.log('a is 4')
-    break  
-  default:
-    console.log('rest....')
+for (let i = 0; i < 3; i += 1){
+  console.log(i)
 }
+console.log(ulEl)
 
-// if (a === 0) {
-//   console.log('a is 0')
-// } else if (a === 2) {
-//   console.log('a is 2')
-// } else if (a === 4) {
-//   console.log('a is 4')
-// } else {
-//   console.log('rest....')
-// }
+const ulEl = document.querySelector('ul')
 
+for (let i = 0; i < 10; i += 1){
+  const li = document.createElement('li')
+  li.textContent = `list-${i+1}`
+  if ((i+1) % 2 === 0) {
+    li.addEventListener('click', function () {
+      console.log(li.textContent)
+    })
+  }
+  ulEl.appendChild(li)
+}
