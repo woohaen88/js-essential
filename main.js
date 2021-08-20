203445d2a65eb3546fe9010a415d3d058ce95542
woohaen88 <woohaen88@gmail.com>
2021-08-20 10:25:35 +0900

    3-04-hoisting

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-// 즉시 실행함수
-// IIFE, Immediately-Invoked Function Expression
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
 
 const a = 7
+
+double()
+
+// const double = function () {
+//   console.log(a*2)
+// }
+
 function double() {
-  console.log(a * 2)
+  console.log(a*2)
 }
-
-double();
-
-// 한번 실행하고 말 함수
-(function () {
-  console.log(a*2)
-})();
-
-// 즉시 실행함수와 double함수의 구분이 명확하지 않아서 생긴 error
-
-//recommend
-(function () {
-  console.log(a*2)
-}())
