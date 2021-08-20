b3d944d5f05596d64b122bc262b6a2353c34faf7
woohaen88 <woohaen88@gmail.com>
2021-08-20 10:18:04 +0900

    3-03-IIFE

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-// arrow function
-// () => {} vs function () {}
+// 즉시 실행함수
+// IIFE, Immediately-Invoked Function Expression
 
-const double = function (x) {
-  return x*2
+const a = 7
+function double() {
+  console.log(a * 2)
 }
-console.log('double: ', double(7))
 
-const doubleArrow = (x) => {
-  return x *2
-}
-console.log("doubleArrow,", doubleArrow(7))
+double();
 
-const doubleArrow2 = (x) => x*2
-console.log("doubleArrow2,", doubleArrow2(8))
+// 한번 실행하고 말 함수
+(function () {
+  console.log(a*2)
+})()
 
-const doubleArrow3 = x => x*2
-console.log("doubleArrow3,", doubleArrow3(9))
-
-const doubleArrow4 = x => ({name: 'woohyeon'})
-console.log("doubleArrow4,", doubleArrow4(9))
+// 즉시 실행함수와 double함수의 구분이 명확하지 않아서 생긴 error
