2d6b6ff159ed194ba942c6f86890f81498a0b8d4
woohaen88 <woohaen88@gmail.com>
2021-08-20 11:40:18 +0900

    3-06-callback

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-// timer function
-// setTimeout(함수, 시간): 일정 시간 후 함수 실행
-// setInterval(함수, 시간): 시간 간격마다 함수 실행
-// clearTimeout(): 설정된 Timeout 함수를 종료
-// clearInterval(): 설정된 Interval 함수를 종료
+// Callback
+// 함수의 인수로 사용되는 함수
 
+// setTimeout(함수, 시간)
 
-//timer
-const timer = setTimeout(() => {
-  console.log("Heropy")
-}, 3000) // unit: ms
+let timeout = (cb) => {
+  setTimeout(() => {
+    console.log('woohyeon')
+    cb()
+  }, 3000)
+}
 
-const h1El = document.querySelector('h1')
-h1El.addEventListener('click', () => {
-  clearTimeout(timer)
+timeout(() => {
+  console.log('Done!')
 })
-
-// Interval
-const timer2 = setInterval(() => {
-  console.log("Heropy")
-}, 3000) // unit: ms
-
-const h1El2 = document.querySelector('h1')
-h1El.addEventListener('click', () => {
-  clearInterval(timer)
-})
-
+// console.log('Done')
